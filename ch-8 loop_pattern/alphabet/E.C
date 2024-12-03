259146d0969c79595afa082faff3f7af4978f0d2
sanjana <sanjanadholariya926@gmail.com>
2024-12-04 01:25:55 +0530

    4/12/24

---
+++ ch-8 loop_pattern/alphabet/E.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j;
+
+	clrscr();
+
+	for(i=1; i<=7; i++)
+	{
+		for(j=1; j<=4; j++)
+		{
+			if(i==1 || i==4 || i==7 || j==1)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	getch();
+}
